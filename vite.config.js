0b9a3b3b717b6c39b9f1667a300466c300bc9b4f
uthayan <uthayanu490@gmail.com>
2025-06-01 16:04:52 +0530

    commit message

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/uthayan111/',
+  base: '/uthayan1111/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
